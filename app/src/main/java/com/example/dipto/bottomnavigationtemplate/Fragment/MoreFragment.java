197e05df5d9e8098ec0687207a3a7e506d54913f
imtiaz <programmer.imtiaz@gmail.com>
2017-11-08 06:53:57 +0600

    ADD- fragments for bottom navigations

---
+++ app/src/main/java/com/example/dipto/bottomnavigationtemplate/Fragment/MoreFragment.java
@@ -0,0 +1,28 @@
+package com.example.dipto.bottomnavigationtemplate.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dipto.bottomnavigationtemplate.R;
+
+/**
+ * Created by Dipto on 11/5/2017.
+ */
+
+public class MoreFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+        return view ;
+    }
+}
--- app/src/main/java/com/example/dipto/bottomnavigationtemplate/MainActivity.java
+++ app/src/main/java/com/example/dipto/bottomnavigationtemplate/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.dipto.bottomnavigationtemplate;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.dipto.bottomnavigationtemplate.Fragment.BurgerFragment;
+import com.example.dipto.bottomnavigationtemplate.Fragment.DrinksFragment;
+import com.example.dipto.bottomnavigationtemplate.Fragment.MoreFragment;
+import com.example.dipto.bottomnavigationtemplate.Fragment.PizzaFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_pizza:
+                    PizzaFragment pizzaFragment = new PizzaFragment();
+                    FragmentTransaction pizza_ft = getSupportFragmentManager().beginTransaction();
+                    pizza_ft.replace(R.id.new_container, pizzaFragment, "Burger Fragment");
+                    pizza_ft.commit();
+                    return true;
+
+                case R.id.action_burger:
+                    BurgerFragment burgerFragment = new BurgerFragment();
+                    FragmentTransaction burger_ft = getSupportFragmentManager().beginTransaction();
+                    burger_ft.replace(R.id.new_container, burgerFragment, "Burger Fragment");
+                    burger_ft.commit();
+                    return true;
+
+                case R.id.action_drink:
+                    DrinksFragment drinksFragment = new DrinksFragment();
+                    FragmentTransaction drinks_ft = getSupportFragmentManager().beginTransaction();
+                    drinks_ft.replace(R.id.new_container, drinksFragment, "Burger Fragment");
+                    drinks_ft.commit();
+                    return true;
+
+                case R.id.action_more:
+                    MoreFragment moreFragment = new MoreFragment();
+                    FragmentTransaction more_ft = getSupportFragmentManager().beginTransaction();
+                    more_ft.replace(R.id.new_container, moreFragment, "Burger Fragment");
+                    more_ft.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 }
